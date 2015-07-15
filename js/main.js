eb602ccc374d7d47ff55eb505de477f0321da0b7
Nutan Ganaraj <nutanmungila@gmail.com>
2015-07-15 15:59:28 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,15 +6,3 @@ React.render(
   <Dropdown />,
   document.body
 );
-
-// hello-world.jsx
- 
-// import React from 'react';
- 
-// class HelloWorld extends React.Component {
-//   render() {
-//     return <div>Hello, qqq!</div>;
-//   }
-// }
- 
-// React.render(<HelloWorld/>,document.getElementById("div1"));
